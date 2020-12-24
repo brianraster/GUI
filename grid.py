09d8158b0d217bbb7d55604146c5b354d75c6413
brianraster <brianaster85@gmail.com>
2020-12-24 08:50:07 -0600

    creating labels and putting them in the window

--- grid.py
+++ grid.py
@@ -2,9 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-# Creating a label widget
-myLabel = Label(root, text="Hello World")
+# Creating a label widget and
+myLabel1 = Label(root, text="Hello World")
+myLabel2 = Label(root, text="My name is Brian")
+myLabel3 = Label(root, text="random string of text")
+
 # packing it onto the screen
-myLabel.pack()
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+myLabel3.grid(row=2, column=7)
 
 root.mainloop()
