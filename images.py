279e271ab0bbc7104f6a8f4ed540e80eaa0876dd
Brian Aster <brianaster85@gmail.com>
2020-12-28 07:28:43 -0600

    add exit button and an image displayed

--- images.py
+++ images.py
@@ -1,8 +1,15 @@
 from tkinter import *
-
+from PIL import ImageTk, Image
+# loa: 1:24
 root = Tk()
 root.title('Working with images')
 root.iconbitmap('dot.ico')
 
+myImg = ImageTk.PhotoImage(Image.open('heart.png'))
+label = Label(image=myImg)
+label.pack()
+
+quitButton = Button(root, text='Exit Program', command=root.quit)
+quitButton.pack()
 
 root.mainloop()
